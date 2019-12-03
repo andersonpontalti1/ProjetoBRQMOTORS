b3891ef6960ee1a688d02f5d1793910f7397b258
Anderson Altomare Pontalti <andersonpontalti@brq.com>
2019-12-03 11:12:21 -0300

    apos atualizacao de transicao de tela foi testado o layout

--- app/src/main/java/br/com/brq/ecc/brqmotors/LoginActivity.kt
+++ app/src/main/java/br/com/brq/ecc/brqmotors/LoginActivity.kt
@@ -1,13 +1,20 @@
 package br.com.brq.ecc.brqmotors
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(baseContext,PrivacidadeActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
