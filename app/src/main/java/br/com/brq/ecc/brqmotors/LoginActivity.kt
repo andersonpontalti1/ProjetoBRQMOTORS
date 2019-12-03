3e83a1bd3634620faa81a96d2f745fcc2c731343
Anderson Altomare Pontalti <andersonpontalti@brq.com>
2019-12-03 10:50:44 -0300

    transicao de tela

--- app/src/main/java/br/com/brq/ecc/brqmotors/LoginActivity.kt
+++ app/src/main/java/br/com/brq/ecc/brqmotors/LoginActivity.kt
@@ -1,13 +1,33 @@
 package br.com.brq.ecc.brqmotors
 
+
+import android.app.Dialog
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main_login.*
 
 class LoginActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
 
+        buttonLogin.setOnClickListener {
+            val intent = Intent(baseContext, PrivacidadeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
+
+
+
+
+
+
+
